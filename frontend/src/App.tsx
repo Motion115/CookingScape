2194444f8d5b8a20266040903c972b05cea8eda4
Motion115 <101267627+Motion115@users.noreply.github.com>
2024-03-29 13:47:16 +0800

    Chore: Init frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Layout, ConfigProvider, Typography, Row, Col } from 'antd';
+import THEME from './looks/theme';
+import VideoPlayer from './components/videoPlayer';
+
+const { Header, Content, Footer } = Layout;
+const { Text } = Typography;
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <ConfigProvider theme={THEME}>
+        <Header>
+          <Text style={{ fontSize: "20px", padding: "2%", fontWeight: "bold" }}>
+            CookingNavigator
+          </Text>
+        </Header>
+        <Content style={{ padding: "2%", margin: "0 auto" }}>
+          <Layout dir="vertical">
+            <Row>
+              <Col span={12}>
+                <VideoPlayer />
+                
+              </Col>
+              <Col span={12}>
+                Ingredient canvas
+              </Col>
+            </Row>
+            <Row>
+              <Col span={12}>
+                Step-by-step canvas
+              </Col>
+              <Col span={12}>
+                milestone canvas
+              </Col>
+            </Row>
+          </Layout>
+        </Content>
+        <Footer style={{ height: "80px", padding: "2%", margin: "0 auto" }}>
+          © This project is in developmenet stage. All rights reserved.
+        </Footer>
+      </ConfigProvider>
+    </div>
+  );
+}
+
+export default App;
--- method/LM_pipeline.py
+++
@@ -1,5 +0,0 @@
-from utils.LM_hook import GLM
-
-def run():
-    glm = GLM("./utils/api_config.json")
-    print("Hello")
